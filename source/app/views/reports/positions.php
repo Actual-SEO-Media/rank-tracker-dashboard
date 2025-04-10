7e37dd4eadb55595209fc45b46c19c4ea4ccecea
quiambao970 <quiambao970@gmail.com>
2025-04-10 14:57:32 +0800

    work

--- source/app/views/reports/positions.php
+++ source/app/views/reports/positions.php
@@ -16,7 +16,7 @@
         </div>
     </div>
 </div>
-<div class="flex items-center mb-6 print-hidden">
+<div class="flex items-center mb-6 print-hidden relative">
      <a href="<?php echo $_ENV['SITE_URL']; ?>/details/<?php echo $report['report_id']; ?>" class="bg-white rounded-md p-2 mr-2 hover:bg-gray-100">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
@@ -28,6 +28,7 @@
         echo date('F Y', strtotime($period . '-01'));
         ?>
     </h1>
+    <button id="report-print-btn" class="inline-flex items-center px-4 py-2 rounded-md bg-white border border-slate-200 shadow-sm text-slate-700 hover:bg-slate-50 transition-colors absolute top-0 right-0"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 text-slate-500" viewBox="0 0 512 512" fill="currentColor"><path d="M128 0C92.7 0 64 28.7 64 64l0 96 64 0 0-96 226.7 0L384 93.3l0 66.7 64 0 0-66.7c0-17-6.7-33.3-18.7-45.3L400 18.7C388 6.7 371.7 0 354.7 0L128 0zM384 352l0 32 0 64-256 0 0-64 0-16 0-16 256 0zm64 32l32 0c17.7 0 32-14.3 32-32l0-96c0-35.3-28.7-64-64-64L64 192c-35.3 0-64 28.7-64 64l0 96c0 17.7 14.3 32 32 32l32 0 0 64c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-64zM432 248a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>Print</button>
 </div>
 <div class="bg-white rounded-lg shadow-md p-6 mb-6 print-no-padding">
     <!-- Tabs for different search engines -->
@@ -351,6 +352,10 @@ function sortTable(tableId, columnIndex, isNumeric = false) {
         }
     }
 }
+
+document.getElementById('report-print-btn').addEventListener('click', () => {
+    window.print();
+});
 </script>
 
 <?php include __DIR__ . '/../layout/footer.php'; ?>
